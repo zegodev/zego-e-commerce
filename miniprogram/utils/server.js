03192ec4a24f56f2ca17d5723eb1f843c20efe5c
haleycai <haleycai@zego.im>
2020-03-30 12:03:59 +0800

    comment

--- miniprogram/utils/server.js
+++ miniprogram/utils/server.js
@@ -29,10 +29,10 @@ function _request({url, data = {}, testMode = true}) {
 
 function getLoginToken(userID, appid) {
     // let { tokenURL,cgi_token, appSign, tokenURL2 } = getApp().globalData;
-    let tokenURL = "https://wssliveroom-demo.zego.im/token";
-    let appSign = '';
+    let tokenURL = "https://wssliveroom-demo.zego.im/token";  // 即构demo 获取token的专用接口，开发者请填写appSign 使用临时接口
+    let appSign = ''; // 填写即构控制台获取的appSign
     let cgi_token = '';
-    let tokenURL2 = "https://sig-wstoken.zego.im:8282/tokenverify";
+    let tokenURL2 = "https://sig-wstoken.zego.im:8282/tokenverify"; // 即构提供的测试环境的测试接口，正式环境要由业务服务端实现
     if (appSign) {
         console.log('>>> get token first type')
         const now = new Date().getTime();
